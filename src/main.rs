3cf9bb14355304aee3ebb5da68e0df66649cc819
Thorben Below <thorbenbelow@gmail.com>
2022-11-13 11:36:54 +0100

    open nic with tun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to cr");
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
